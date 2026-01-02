d62aa6019886d146fb7727e3777b85ad22c00493
engsr6982 <pwxg286704@163.com>
2026-01-03 04:08:32 +0800

    feat: add vite-plugin-vue-devtools dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 import { fileURLToPath, URL } from 'node:url'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue(), crx({ manifest })],
+  plugins: [vue(), crx({ manifest }), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
